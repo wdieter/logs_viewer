a2b2e35b63c029240f55433164cca200181f9454
wdieter <wdieterich@gmail.com>
2024-11-06 17:10:55 -0700

    first commit, add domain.py, domain tests

---
+++ domain.py
@@ -0,0 +1,107 @@
+import os
+from typing import List, Callable
+from dataclasses import dataclass
+
+LOG_DIRECTORY = '/var/log'
+NOT_SUPPORTED_FILE_TYPE_ERROR = "Error reading file: "
+
+@dataclass
+class File:
+    file_name: str
+    logs: List[str]
+
+    def to_dict(self) -> dict:
+        file_dict = {
+            'file_name': self.file_name,
+            'logs': self.logs
+        }
+        return file_dict
+
+
+def get_logs(filename: str, n: int, keyword: str, read_file_func: Callable[[str], List[str]]) -> List[File]:
+    """
+
+    :param filename: either a file or a directory. if a directory is passed, this function will
+        call itself recursively to get all logs from all files under that directory
+    :param n: the number of lines to return from each file. Note this does not limit how many logs are
+        returned if a directory is passed under filename
+    :param keyword: filters log results to lines containing this keyword, case insensitive
+    :param read_file_func: a function that takes a filename and produces a list of strings. injecting this dependency
+        makes the function much easier to test and makes this function easier to extend
+    :return: A list of File objects that match the query parameters.
+    """
+    list_files = []
+    filepath = os.path.join(LOG_DIRECTORY, filename)
+    if os.path.isfile(filepath):
+        logs: List[str] = tail_log_file(filepath, read_file_func)
+        filtered_logs = filter_logs(logs, keyword, n)
+        if len(filtered_logs) > 0:
+            list_files.append(File(file_name=filepath, logs=filtered_logs))
+    elif os.path.isdir(filepath):
+        for file in get_all_files(filepath):
+            logs: List[File] = get_logs(file, n, keyword, read_file_func)
+            list_files.extend(logs)
+    else:
+        raise FileNotFoundError
+    return list_files
+
+# todo add test
+def tail_log_file(filepath: str, filereader: Callable[[str], List[str]]) -> List[str]:
+    """
+    Returns the last n lines from the log file.
+    """
+
+    try:
+        raw_lines = filereader(filepath)
+    except (OSError, UnicodeDecodeError) as error:
+        return [f"{NOT_SUPPORTED_FILE_TYPE_ERROR} {error}"]
+
+    # remove new lines from logfile, and new line from the log entries
+    cleaned_logs = [log.strip() for log in raw_lines if log != "\n"]
+
+    # return most recent logs first
+    cleaned_logs.reverse()
+
+    return cleaned_logs
+
+# todo add test
+def filter_logs(log_entries: List[str], keyword: str, n: int) -> List[str]:
+    """
+    Remove logs that don't contain the keyword or the NOT_SUPPORTED_FILE_TYPE_ERROR
+    and limit number of logs by n
+    """
+    res = []
+    for line in log_entries:
+        append = False
+        if NOT_SUPPORTED_FILE_TYPE_ERROR in line:
+            append = True
+        if keyword == "":
+            append = True
+        elif case_insensitive_contains(line, keyword):
+            append = True
+
+        if append:
+            res.append(line)
+
+    limit_logs_by_n = res[:n]
+    return limit_logs_by_n
+
+
+# Utils below
+
+def case_insensitive_contains(text: str, substring: str) -> bool:
+    return substring.lower() in text.lower()
+
+
+def read_file(filepath: str) -> List[str]:
+    with open(filepath, 'r') as file:
+        return file.readlines()
+
+
+def get_all_files(directory: str):
+    file_list = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            file_list.append(os.path.join(root, file))
+    return file_list
+
---
+++ test_domain.py
@@ -0,0 +1,82 @@
+from pyfakefs.fake_filesystem_unittest import TestCase
+import domain
+
+# Test constants
+LINE_1_WITH_KEYWORD = "line1 with keyword"
+LINE_2_WITHOUT = "line2 without"
+LINE_3_WITH = "line3 with KEYWORD"
+SUBDIR = "subdir"
+SUBDIR2 = "subdir2"
+FILE_LOG = "file.log"
+SUBDIR2_FILE_5 = f"/var/log/{SUBDIR}/{SUBDIR2}{FILE_LOG}.5"
+SUBDIR2_FILE_5_CONTENTS = f"{SUBDIR2} {LINE_1_WITH_KEYWORD}\n{SUBDIR2} {LINE_2_WITHOUT}\n{SUBDIR2} {LINE_3_WITH}\n"
+
+
+class Test(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.setUpClassPyfakefs()
+        # setup the fake filesystem using standard functions
+        cls.fake_fs().create_file(
+            f"/var/log/{FILE_LOG}",
+            contents=f"{LINE_1_WITH_KEYWORD}\n{LINE_2_WITHOUT}\n{LINE_3_WITH}\n"
+        )
+        cls.fake_fs().create_file(
+            f"/var/log/{FILE_LOG}.2",
+            contents=f"2 {LINE_1_WITH_KEYWORD}\n2 {LINE_2_WITHOUT}\n2 {LINE_3_WITH}\n"
+        )
+        cls.fake_fs().create_file(
+            f"/var/log/{SUBDIR}/{FILE_LOG}.3",
+            contents=f"{SUBDIR} {LINE_1_WITH_KEYWORD}\n{SUBDIR} {LINE_2_WITHOUT}\n{SUBDIR} {LINE_3_WITH}\n"
+        )
+        cls.fake_fs().create_file(
+            f"/var/log/{SUBDIR}/{SUBDIR2}{FILE_LOG}.4",
+            contents=f"{SUBDIR2} {LINE_1_WITH_KEYWORD}\n{SUBDIR2} {LINE_2_WITHOUT}\n{SUBDIR2} {LINE_3_WITH}\n"
+        )
+        cls.fake_fs().create_file(
+            SUBDIR2_FILE_5,
+            contents=SUBDIR2_FILE_5_CONTENTS
+        )
+
+    def test_get_logs_one_file(self):
+        res = domain.get_logs(filename=FILE_LOG, n=2, keyword="", read_file_func=domain.read_file)
+        expected = [LINE_3_WITH, LINE_2_WITHOUT]  # reverse list (not in place) and take first 2 items
+        self.assertEqual(1, len(res))
+        self.assertEqual(expected, res[0].logs)
+
+    def test_get_logs_one_file_keyword_filter(self):
+        res = domain.get_logs(filename=FILE_LOG, n=2, keyword="keyword", read_file_func=domain.read_file)
+        expected = [LINE_3_WITH, LINE_1_WITH_KEYWORD]
+        self.assertEqual(1, len(res))
+        self.assertEqual(expected, res[0].logs)
+
+    def test_get_logs_one_file_no_filter_all_records(self):
+        res = domain.get_logs(filename=FILE_LOG, n=100, keyword="", read_file_func=domain.read_file)
+        expected = [LINE_3_WITH, LINE_2_WITHOUT, LINE_1_WITH_KEYWORD]
+        self.assertEqual(1, len(res))
+        self.assertEqual(expected, res[0].logs)
+
+    def test_get_logs_all_files_no_filter_all_records(self):
+        res = domain.get_logs(filename="", n=100, keyword="", read_file_func=domain.read_file)
+        expected = [LINE_3_WITH, LINE_2_WITHOUT, LINE_1_WITH_KEYWORD]
+        self.assertEqual(5, len(res))
+        self.assertEqual(expected, res[0].logs)
+        for file in res:
+            if file.file_name == SUBDIR2_FILE_5:
+                expected_list = SUBDIR2_FILE_5_CONTENTS.splitlines(keepends=False)
+                expected_list.reverse()
+                self.assertEqual(file.logs, expected_list)
+
+    def test_get_logs_all_files_filter(self):
+        res = domain.get_logs(filename="", n=2, keyword="keyword", read_file_func=domain.read_file)
+        expected = [LINE_3_WITH, LINE_1_WITH_KEYWORD]
+        self.assertEqual(5, len(res))
+        self.assertEqual(expected, res[0].logs)
+        for file in res:
+            if file.file_name == SUBDIR2_FILE_5:
+                expected_list = SUBDIR2_FILE_5_CONTENTS.splitlines(keepends=False)
+                expected_list.reverse()
+                expected_list.pop(1)  # remove middle item
+                self.assertEqual(file.logs, expected_list)
+
+
